8e35aecb0a8e7affbba096001fc0e62fffcbfab0
AFRID SHAIK <afridayan01@gmail.com>
2023-09-07 11:43:08 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import uvicorn
 import smtplib
 import random
 app = FastAPI()
@@ -170,3 +171,6 @@ Subject: {subject}
         "email":email,
         "otp":otp
     }
+    
+if __name__ == "__main__":
+  uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
