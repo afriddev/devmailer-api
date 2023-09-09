24a060c9e84b256139cbde62b9c38d8f621b13d0
AFRID SHAIK <afridayan01@gmail.com>
2023-09-09 06:05:01 +0000

    commit

--- main.py
+++ main.py
@@ -172,3 +172,6 @@ def test(data: emailRequestModel):
         return {
             "message": "wrongEmail"
         }
+
+if __name__ == "__main__":
+  uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
