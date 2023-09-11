3296846493d689d8bee528c89851eaea738eb907
AFRID SHAIK <afridayan01@gmail.com>
2023-09-11 04:41:43 +0000

    Update main.py

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ from model import emailRequestModel
 from server import defaultEmail, defaultPasskey
 from sendMethods import sendCustom
 import re
+import requests
 
 """
 
@@ -83,7 +84,7 @@ Root Of The EmailAPI
 def root():
     return {
         "message":{
-            "serverStatus":"running",
+            "serverStatus":"running..",
             "version":"1.1.3"
         }
     }
@@ -130,7 +131,7 @@ def test(data: emailRequestModel):
         else:
             if (data.fromEmail != defaultEmail and data.passkey == None):
                 return {
-                    "message": "passkeyRequired"
+                    "message": "passkeyrequired"
                 }
             elif (data.fromEmail == defaultEmail):
                 try:
@@ -172,6 +173,68 @@ def test(data: emailRequestModel):
         return {
             "message": "wrongEmail"
         }
+
+@app.get("/{toEmail}")
+def onlyEmail(toEmail:str):
+    emailServer = "https://emailsender.cyclic.cloud/sendEmail"
+    responseFromServer = requests.post(emailServer,
+    json={
+        "toEmail":toEmail
+    }
+    ) 
+    return responseFromServer.json()
+
+@app.get("/{toEmail}/{otp}")
+def sendOtp(toEmail:str,otp:int):
+    emailServer = "https://emailsender.cyclic.cloud/sendEmail"
+    responseFromServer = requests.post(emailServer,
+    json={
+        "toEmail":toEmail,
+        "body":"Your Verification Code Is - "+str(otp)
+    }
+    ) 
+    return responseFromServer.json()
+@app.get("/{toEmail}/{title}/{subject}/{body}")
+def sendMessage(toEmail:str,title:str,subject:str,body:str):
+    emailServer = "https://emailsender.cyclic.cloud/sendEmail"
+    responseFromServer = requests.post(emailServer,
+    json={
+        "toEmail":toEmail,
+        "title":title,
+        "subject":subject,
+        "body":body
+    }
+    ) 
+    return responseFromServer.json()
+@app.get("/{fromEmail}/{passkey}/{toEmail}/{title}/{subject}/{body}")
+def customMessage(
+    fromEmail:str,
+    passkey:str,
+    toEmail:str,
+    title:str,
+    subject:str,
+    body:str,):
+    emailServer = "https://emailsender.cyclic.cloud/sendEmail"
+    responseFromServer = requests.post(emailServer,
+    json={
+        "fromEmail":fromEmail,
+        "passkey":passkey,
+        "toEmail":toEmail,
+        "title":title,
+        "subject":subject,
+        "body":body
+    }
+    ) 
+    return responseFromServer.json()
+
+
+
+    
+
+
+
+
+
 
 if __name__ == "__main__":
   uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
