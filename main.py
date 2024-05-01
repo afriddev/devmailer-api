eb79c1e496ece5ff46cfc7ef6a2e7291f0e2fbfc
SHIAK AFRID <afridayan01@gmail.com>
2024-05-01 11:32:23 +0530

    updated cors config

--- main.py
+++ main.py
@@ -42,6 +42,24 @@ initalize fastapi
 
 """
 app = FastAPI()
+"""
+cors config
+"""
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 
 """
